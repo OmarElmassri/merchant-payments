2e51f2b7e8fda362a952cac72646ecbce97597f9
Omar Elmassri <okmassri@gmail.com>
2022-04-22 11:45:54 +0200

    setup swagger and body-parser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json, urlencoded } from 'body-parser';
+import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+
+  // JSON size
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
+
+  // Swagger setup
+  const options = new DocumentBuilder()
+    .setTitle('Merchant Payments API')
+    .setDescription('API docs for Merchant Payments')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  // Class validator setup
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
